6c9a61daeae6b3de30ddaecf738d671a4ec92836
flowdas <flowdas@gmail.com>
2021-03-04 12:35:10 +0900

    license notification

--- typeable/object.py
+++ typeable/object.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2021 Flowdas Inc. & Dong-gweon Oh <prospero@flowdas.com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from collections.abc import Mapping
 from .typing import get_type_hints
 
