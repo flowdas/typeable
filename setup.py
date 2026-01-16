b7e9bb4e0db399b981f581073b976d5321f9d2da
flowdas <flowdas@gmail.com>
2026-01-16 01:58:39 +0000

    python 3.9~3.14 support

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup_requires = [
 ]
 
 install_requires = [
-    'typing-extensions;python_version<"3.9"',
 ]
 
 tests_require = [
@@ -42,7 +41,7 @@ setup(
     entry_points={},
     zip_safe=True,
     keywords=('validation', 'modeling', 'schema', 'typing', 'serialization', 'type casting'),
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -51,9 +50,12 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Topic :: Internet',
         'Topic :: Utilities',
         'Topic :: Software Development :: Libraries :: Python Modules',
