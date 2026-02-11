4050d5c871a2a02f584cc475e4cb8396cb5534ce
flowdas <flowdas@gmail.com>
2026-02-11 03:47:24 +0000

    longest error location for Union

--- typeable/_casters/union.py
+++ typeable/_casters/union.py
@@ -1,20 +1,29 @@
+from contextlib import ExitStack
 import sys
 from types import UnionType
 from typing import Union
 
 from .._deepcast import DeepCast, deepcast
+from .._error import capture, traverse
 
 
 @deepcast.register
 def UnionType_from_object(deepcast: DeepCast, cls: type[UnionType], val: object, *Ts):
     uc = deepcast.get_unioncast(Ts)
+    deepest = ()
     for T in uc.dispatch(val.__class__):
         try:
-            return deepcast(T, val)
+            with capture() as error:
+                return deepcast(T, val)
         except Exception:
+            if error.location and len(error.location) > len(deepest):
+                deepest = error.location
             continue
     else:
-        raise TypeError("no match")
+        with ExitStack() as stack:
+            for loc in deepest:
+                stack.enter_context(traverse(loc))
+            raise TypeError("no match")
 
 
 if sys.version_info < (3, 14):
